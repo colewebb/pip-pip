51eb56a540ce69dcabe65109e414ad7ca1151356
Cole Webb <cole.webb@outlook.com>
2017-01-06 01:31:17 -0800

    push before bed

--- pip-pip.py
+++ pip-pip.py
@@ -2,13 +2,14 @@ from os import getenv
 from subprocess import call
 pwd = getenv("PWD")
 try:
-	index = open(pwd+"/index.html")
+	index = open(pwd+"/template.html")
 	index.close
 	db = open(pwd+"/main-db")
 except:
 	print("oops")
 index = open(pwd+"/index.html","a")
 increment=0
+call("cp "+pwd+"/template.html "+pwd+"/index.html",shell=True)
 for line in db:
 	line=line.rstrip()
 	line_pieces=line.split("=",2)
@@ -18,4 +19,7 @@ for line in db:
 	tertiary=open(pwd+"/"+str(increment)+".html", "w")
 	tertiary.write("<head><title>"+line_pieces[0]+"</title><link rel='stylesheet' href='main.css'><link href='https://fonts.googleapis.com/css?family=Ubuntu' rel='stylesheet'></head>")
 	tertiary.write("<iframe width='560' height='315' src='https://www.youtube.com/embed/videoseries?list="+line_pieces[2]+"' frameborder='0' allowfullscreen></iframe>")
+	tertiary.write("<p><a href='file://"+pwd+"/index.html'>Back</a></p>")
 	index.write("<p><a href='file://"+pwd+"/"+str(increment)+".html'>"+name+"</a></p>")
+final=raw_input("Compiled without errors. Cool. Press return to continue. ")
+call("chromium-browser --window-size=575x330 --app='file://"+pwd+"/index.html'", shell=True)
