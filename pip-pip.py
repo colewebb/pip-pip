7e9afaa011112e3b1b0b347fbc3d0c7d4fb7f75e
Cole Webb <cole.webb@outlook.com>
2017-01-06 13:04:20 -0800

    Success! Script is complete, more or less. Hello functional...

--- pip-pip.py
+++ pip-pip.py
@@ -7,9 +7,9 @@ try:
 	db = open(pwd+"/main-db")
 except:
 	print("oops")
+call("cp "+pwd+"/template.html "+pwd+"/index.html",shell=True)
 index = open(pwd+"/index.html","a")
 increment=0
-call("cp "+pwd+"/template.html "+pwd+"/index.html",shell=True)
 for line in db:
 	line=line.rstrip()
 	line_pieces=line.split("=",2)
@@ -21,5 +21,6 @@ for line in db:
 	tertiary.write("<iframe width='560' height='315' src='https://www.youtube.com/embed/videoseries?list="+line_pieces[2]+"' frameborder='0' allowfullscreen></iframe>")
 	tertiary.write("<p><a href='file://"+pwd+"/index.html'>Back</a></p>")
 	index.write("<p><a href='file://"+pwd+"/"+str(increment)+".html'>"+name+"</a></p>")
+index.close()
 final=raw_input("Compiled without errors. Cool. Press return to continue. ")
 call("chromium-browser --window-size=575x330 --app='file://"+pwd+"/index.html'", shell=True)
